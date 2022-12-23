d7090664e840f55b2f65cb01affe26daa1a53138
Fluke <adisorn.sjr@gmail.com>
2022-12-23 16:54:24 +0700

    completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,48 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+    int age;
+    int H;
+    int head;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> H;
+            if(H < 100){
+                character = "Chopper";
+            }else if(H<180){
+                character = "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> head;
+                if(head>1100000000){
+                    character = "Zoro";
+                }else{
+                    character = "Sanji";
+                    }
+     
+            }
+
+    }else if(age>26 and age<=60){
+        cout << "Enter your bounty: ";
+        cin >> head;
+        if(head>500000000){
+            character = "Jinbe";
+        }else{ 
+            character = "Franky";}
+    }else{
+        character = "Brook";
+    }
+
+    
+cout << "Your character = " << character;
+return 0;
+}
